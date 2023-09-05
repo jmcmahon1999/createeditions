33fcab2c6e421b1b7ca2fe51ff0bcb073719f91a
Joe McMahon <jmcmahon1999@gmail.com>
2023-09-05 07:39:52 +0100

    Cleaning up leftovers from template mod

--- src/main/java/net/ducko/createeditions/mixin/BacktankBlockEntityMixin.java
+++ src/main/java/net/ducko/createeditions/mixin/BacktankBlockEntityMixin.java
@@ -3,12 +3,8 @@ package net.ducko.createeditions.mixin;
 import com.simibubi.create.AllBlocks;
 import com.simibubi.create.AllItems;
 import com.simibubi.create.content.equipment.armor.BacktankBlockEntity;
-import com.simibubi.create.content.kinetics.base.KineticBlockEntity;
 import com.simibubi.create.infrastructure.config.AllConfigs;
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.Nameable;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,20 +13,17 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import static com.simibubi.create.content.equipment.armor.BacktankUtil.maxAir;
-
 @Mixin(BacktankBlockEntity.class)
-public abstract class BacktankBlockEntityMixin extends KineticBlockEntity implements Nameable  {
+public abstract class BacktankBlockEntityMixin  {
 
-    public BacktankBlockEntityMixin(BlockEntityType<?> typeIn, BlockPos pos, BlockState state) {
-        super(typeIn, pos, state);
-    }
-
-
-    @Shadow
+    @Shadow(
+            remap = false
+    )
     private int capacityEnchantLevel;
 
-    @Shadow private Component defaultName;
+    @Shadow(
+            remap = false
+    ) private Component defaultName;
 
     @Inject(
             method = "getDefaultName",
@@ -38,7 +31,7 @@ public abstract class BacktankBlockEntityMixin extends KineticBlockEntity implem
             remap = false,
             cancellable = true
     )
-    private static void getDefaultName(BlockState state, CallbackInfoReturnable<Component> callback) {
+    private static void injectGetDefaultName(BlockState state, CallbackInfoReturnable<Component> callback) {
         if (AllBlocks.NETHERITE_BACKTANK.has(state)) {
             callback.setReturnValue(AllItems.NETHERITE_BACKTANK.get().getDescription());
         }
--- src/main/java/net/ducko/createeditions/mixin/BacktankUtilMixin.java
+++ src/main/java/net/ducko/createeditions/mixin/BacktankUtilMixin.java
@@ -3,9 +3,7 @@ package net.ducko.createeditions.mixin;
 import com.simibubi.create.AllEnchantments;
 import com.simibubi.create.content.equipment.armor.BacktankUtil;
 import com.simibubi.create.infrastructure.config.AllConfigs;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,7 +16,7 @@ import static com.simibubi.create.content.equipment.armor.BacktankUtil.maxAir;
 public abstract class BacktankUtilMixin {
 
     @Inject(
-            method = "maxAir",
+            method = "maxAir(Lnet/minecraft/world/item/ItemStack;)I",
             at = @At("HEAD"),
             remap = false,
             cancellable = true
